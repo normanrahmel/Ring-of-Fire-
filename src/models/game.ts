50ddc7f6e4ce3658062aade77d5f4a0ac7f05876
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-07-28 18:09:43 +0200

    added new Players

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-  public players: string[] = [];
+  public players: string[] = ['Hans', 'Rolf', 'Affe'];
   public stack: string[] = [];
   public playedCards: string[] = [];
   public currenPlayer: number = 0;
